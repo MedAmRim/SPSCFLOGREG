de42037a2ed3738077794ca8acb0d19b1198d239
MedAmRim <mejdaoui.z@gmail.com>
2022-10-21 10:57:36 +0000

    Med_3

--- SPSC/demo/src/main/java/com/example/demo/Security/securityConfiguration/WebSecurityConfiguration.java
+++ SPSC/demo/src/main/java/com/example/demo/Security/securityConfiguration/WebSecurityConfiguration.java
@@ -1,9 +1,54 @@
 package com.example.demo.Security.securityConfiguration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.example.demo.ApplicationUser.ApplicationUser;
+import com.example.demo.ApplicationUser.ApplicationUserService;
+
+import lombok.AllArgsConstructor;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    private final ApplicationUserService applicationUserServicer;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // TODO Auto-generated method stub
+
+        http
+        .csrf().disable()
+        .authorizeRequests()
+            .antMatchers("/api/v*/registration/**")
+            .permitAll()
+        .anyRequest()
+        .authenticated().and()
+        .formLogin();
+    }
     
-    
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+    DaoAuthenticationProvider provider =
+            new DaoAuthenticationProvider();
+    provider.setPasswordEncoder(bCryptPasswordEncoder);
+    provider.setUserDetailsService(applicationUserServicer);
+    return provider;
+}
+
 }
